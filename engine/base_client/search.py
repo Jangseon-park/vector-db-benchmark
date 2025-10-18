da88f534546e9e4267444c20bdfe3a43cc54a1dc
Jangseon-Park <jap036@ucsd.edu>
2025-10-18 23:53:26 +0000

    <function> add m5 workbegin

--- engine/base_client/search.py
+++ engine/base_client/search.py
@@ -82,7 +82,8 @@ class BaseSearcher:
             results = []
             for i, query in enumerate(tqdm.tqdm(queries)):
                 if not switched and i == switch_point:
-                    os.system('echo "---- GEM5 SWITCH POINT ----"')
+                    #os.system('echo "---- GEM5 SWITCH POINT ----"')
+                    os.system("m5 workbegin")
                     switched = True
                 results.append(search_one(query))
             precisions, latencies = list(zip(*results))
